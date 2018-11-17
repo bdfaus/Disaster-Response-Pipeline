92c0468e4a39bd9b48dce94a46eca029d244e8de
BDFOZ <Ben.Faus@live.com>
2018-11-17 15:14:17 -0600

    Refactor: Notebooks to scripts
    Scripts for ETL and ML now exist. Both still require clean-up.

---
+++ models/train_classifier.py
@@ -0,0 +1,71 @@
+import sys
+from sqlalchemy import create_engine
+import pandas as pd
+import numpy as np
+
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.decomposition import TruncatedSVD
+from sklearn.metrics import classification_report, hamming_loss
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+#nltk for tokenizing/lemmatizing
+import nltk
+nltk.download(['punkt','wordnet'])
+
+
+
+def load_data(database_filepath):
+    engine = create_engine('sqlite:///' + str(database_filepath))
+    df = pd.read_sql_table('MessageTable',con=engine)
+
+
+def tokenize(text):
+    pass
+
+
+def build_model():
+    pass
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    pass
+
+
+def save_model(model, model_filepath):
+    pass
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
